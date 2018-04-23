d46a32ffae1694359cbb6c827b1578aebdfcaf98
unknown <rifardity@gmail.com>
2018-04-24 00:54:18 +0700

    First File Add

---
+++ src/router/index.js
@@ -0,0 +1,181 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+const _import = require('./_import_' + process.env.NODE_ENV)
+// in development-env not use lazy-loading, because lazy-loading too many pages will cause webpack hot update too slow. so only in production use lazy-loading;
+// detail: https://panjiachen.github.io/vue-element-admin-site/#/lazy-loading
+
+Vue.use(Router)
+
+/* Layout */
+import Layout from '../views/layout/Layout'
+
+/** note: submenu only apppear when children.length>=1
+*   detail see  https://panjiachen.github.io/vue-element-admin-site/#/router-and-nav?id=sidebar
+**/
+
+/**
+* hidden: true                   if `hidden:true` will not show in the sidebar(default is false)
+* alwaysShow: true               if set true, will always show the root menu, whatever its child routes length
+*                                if not set alwaysShow, only more than one route under the children
+*                                it will becomes nested mode, otherwise not show the root menu
+* redirect: noredirect           if `redirect:noredirect` will no redirct in the breadcrumb
+* name:'router-name'             the name is used by <keep-alive> (must set!!!)
+* meta : {
+    roles: ['admin','editor']     will control the page roles (you can set multiple roles)
+    title: 'title'               the name show in submenu and breadcrumb (recommend set)
+    icon: 'svg-name'             the icon show in the sidebar,
+    noCache: true                if true ,the page will no be cached(default is false)
+  }
+**/
+export const constantRouterMap = [
+  { path: '/login', component: _import('login/index'), hidden: true },
+  { path: '/authredirect', component: _import('login/authredirect'), hidden: true },
+  { path: '/404', component: _import('errorPage/404'), hidden: true },
+  { path: '/401', component: _import('errorPage/401'), hidden: true },
+  {
+    path: '',
+    component: Layout,
+    redirect: 'dashboard',
+    children: [{
+      path: 'dashboard',
+      component: _import('dashboard/index'),
+      name: 'dashboard',
+      meta: { title: 'dashboard', icon: 'dashboard', noCache: true }
+    }]
+  }
+]
+
+export default new Router({
+  // mode: 'history', // require service support
+  scrollBehavior: () => ({ y: 0 }),
+  routes: constantRouterMap
+})
+
+export const asyncRouterMap = [
+  {
+    path: '/data-master',
+    component: Layout,
+    redirect: 'noredireect',
+    name: 'dataMaster',
+    meta: {
+      title: 'Data Master',
+      icon: 'tab'
+    },
+    children: [
+      {
+        path: '/data-master/obat',
+        component: _import('dataMaster/obat/obat'),
+        redirect: '/data-master/obat/obat',
+        name: 'Obat',
+        meta: {
+          title: 'Obat',
+          icon: 'table'
+        },
+        children: [
+          { path: 'data-obat', component: _import('dataMaster/obat/obat'), name: 'dataObat', meta: { title: 'Data Obat' }},
+          { path: 'tipe-obat', component: _import('dataMaster/obat/tipe'), name: 'tipeObat', meta: { title: 'Tipe Obat' }},
+          { path: 'lokasi-obat', component: _import('dataMaster/obat/lokasi'), name: 'lokasiObat', meta: { title: 'Lokasi Obat' }}
+        ]
+      },
+      {
+        path: '/data-master/karyawan',
+        component: _import('dataMaster/karyawan/karyawan'),
+        redirect: '/data-master/karyawan/karyawan',
+        name: 'Karyawan',
+        meta: {
+          title: 'Karyawan',
+          icon: 'table'
+        },
+        children: [
+          { path: 'data-karyawan', component: _import('dataMaster/karyawan/karyawan'), name: 'dataKaryawan', meta: { title: 'Data Karyawan' }},
+          { path: 'jabatan-karyawan', component: _import('dataMaster/karyawan/jabatan'), name: 'jabatanKaryawan', meta: { title: 'Jabatan Karyawan' }}
+        ]
+      },
+      { path: '/data-master/suplier', component: _import('dataMaster/suplier/suplier'), name: 'dataSuplier', meta: { title: 'Data Suplier', icon: 'table' }}
+    ]
+  },
+
+  {
+    path: '/form',
+    component: Layout,
+    redirect: 'noredirect',
+    name: 'form',
+    meta: {
+      title: 'form',
+      icon: 'form'
+    },
+    children: [
+      { path: 'create-form', component: _import('form/create'), name: 'createForm', meta: { title: 'createForm', icon: 'table' }},
+      { path: 'edit-form', component: _import('form/edit'), name: 'editForm', meta: { title: 'editForm', icon: 'table' }}
+    ]
+  },
+
+  {
+    path: '/error',
+    component: Layout,
+    redirect: 'noredirect',
+    name: 'errorPages',
+    meta: {
+      title: 'errorPages',
+      icon: '404'
+    },
+    children: [
+      { path: '401', component: _import('errorPage/401'), name: 'page401', meta: { title: 'page401', noCache: true }},
+      { path: '404', component: _import('errorPage/404'), name: 'page404', meta: { title: 'page404', noCache: true }}
+    ]
+  },
+
+  {
+    path: '/error-log',
+    component: Layout,
+    redirect: 'noredirect',
+    children: [{ path: 'log', component: _import('errorLog/index'), name: 'errorLog', meta: { title: 'errorLog', icon: 'bug' }}]
+  },
+
+  {
+    path: '/excel',
+    component: Layout,
+    redirect: '/excel/export-excel',
+    name: 'excel',
+    meta: {
+      title: 'excel',
+      icon: 'excel'
+    },
+    children: [
+      { path: 'export-excel', component: _import('excel/exportExcel'), name: 'exportExcel', meta: { title: 'exportExcel' }},
+      { path: 'export-selected-excel', component: _import('excel/selectExcel'), name: 'selectExcel', meta: { title: 'selectExcel' }},
+      { path: 'upload-excel', component: _import('excel/uploadExcel'), name: 'uploadExcel', meta: { title: 'uploadExcel' }}
+    ]
+  },
+
+  {
+    path: '/zip',
+    component: Layout,
+    redirect: '/zip/download',
+    alwaysShow: true,
+    meta: { title: 'zip', icon: 'zip' },
+    children: [{ path: 'download', component: _import('zip/index'), name: 'exportZip', meta: { title: 'exportZip' }}]
+  },
+
+  {
+    path: '/theme',
+    component: Layout,
+    redirect: 'noredirect',
+    children: [{ path: 'index', component: _import('theme/index'), name: 'theme', meta: { title: 'theme', icon: 'theme' }}]
+  },
+
+  {
+    path: '/clipboard',
+    component: Layout,
+    redirect: 'noredirect',
+    children: [{ path: 'index', component: _import('clipboard/index'), name: 'clipboardDemo', meta: { title: 'clipboardDemo', icon: 'clipboard' }}]
+  },
+
+  {
+    path: '/i18n',
+    component: Layout,
+    children: [{ path: 'index', component: _import('i18n-demo/index'), name: 'i18n', meta: { title: 'i18n', icon: 'international' }}]
+  },
+
+  { path: '*', redirect: '/404', hidden: true }
+]
